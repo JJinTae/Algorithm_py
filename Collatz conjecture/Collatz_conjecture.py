dd726c2ee5a79d5451c7aef92e4fab56417d60f5
JJinTae <emflaxl100@naver.com>
2020-11-14 23:59:25 +0900

    Implement Collatz conjecture

--- Collatz conjecture/Collatz_conjecture.py
+++ Collatz conjecture/Collatz_conjecture.py
@@ -1,3 +1,14 @@
 def solution(num):
-    answer = 0
-    return answer
+    
+    cnt = 0
+    
+    while cnt < 500:
+        if num == 1:
+            return cnt
+        else :
+            if num % 2 == 0:
+                num /= 2
+            else:
+                num = num * 3 + 1
+        cnt += 1
+    return -1
