ef4ce68178c68a1368ed25c34009ecc12576e9f1
JJinTae <emflaxl100@naver.com>
2020-11-15 15:55:32 +0900

    Count Letter

---
+++ 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.py
@@ -0,0 +1,10 @@
+def solution(s):
+    s = s.lower()
+    return s.count('p') == s.count('y')
+    
+    """
+    if s.count('p') == s.count('y'):
+        return True
+    else:
+        return False
+    """
