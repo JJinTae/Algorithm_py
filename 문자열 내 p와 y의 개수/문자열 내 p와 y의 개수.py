8ee81726bafe219ceddf3328bf1b7fc8c87cf151
JJinTae <46085058+JJinTae@users.noreply.github.com>
2020-11-18 23:29:00 +0900

    Update 문자열 내 p와 y의 개수.py

--- 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.py
+++ 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.py
@@ -1,10 +1,3 @@
 def solution(s):
     s = s.lower()
     return s.count('p') == s.count('y')
-    
-    """
-    if s.count('p') == s.count('y'):
-        return True
-    else:
-        return False
-    """
