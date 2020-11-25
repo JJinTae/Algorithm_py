1b3ef99b7f06eccd6b4bcae6a8d432ef16ef7a58
JJinTae <46085058+JJinTae@users.noreply.github.com>
2020-11-25 23:07:51 +0900

    Update 실패율

--- 실패율/실패율.py
+++ 실패율/실패율.py
@@ -1,26 +1,11 @@
-from collections import Counter
-
 def solution(N, stages):
-
-    num = len(stages) # 사람 수를 저장
-    failure = [0] * N # 실패율 저장
-    stages = Counter(stages)
-
-    for i in range(1, N+1):
-        if stages[i] != 0:
-            failure[i-1] = stages[i] / num
-            num -= stages[i]
-
-    answer = []
-    num = len(failure)
-    for i, item in enumerate(failure):
-        m = item
-        idx = i
-        for j, _item in enumerate(failure[::-1]):
-            if _item >= m :
-                    m = _item
-                    idx = num -1 -j
-        answer.append(idx + 1)
-        failure[idx] = -1
-
-    return answer
+    result = {}
+    denominator = len(stages)
+    for stage in range(1, N+1):
+        if denominator != 0:
+            count = stages.count(stage)
+            result[stage] = count / denominator
+            denominator -= count
+        else:
+            result[stage] = 0
+    return sorted(result, key=lambda x : result[x], reverse=True)
