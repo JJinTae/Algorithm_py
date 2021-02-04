e9f4c8835f69d8a71b4638dafb935d50b032bb28
JJinTae <46085058+JJinTae@users.noreply.github.com>
2021-02-04 23:20:48 +0900

    Update 같은 숫자는 싫어.py
    Index로 접근하지 않고 바로 접근하는 코드 추가

--- Level1/같은 숫자는 싫어/같은 숫자는 싫어.py
+++ Level1/같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -2,10 +2,16 @@ def solution(arr):
     answer = []
     # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
 
+    for i in arr:
+        if len(answer) == 0 or i != answer[-1]:
+            answer.append(i)
+
+
+    """
     for i in range(len(arr)- 1) :
         if arr[i] != arr[i+1]:
             answer.append(arr[i])
             
     answer.append(arr[-1])
-    
+    """
     return answer
